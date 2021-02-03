1144f0e71ab04ac9a40fd1a963b49015ab95bcad
张扬 <zhangyang1@liepin.com>
2021-02-03 17:57:57 +0800

    feat: 新增 umd learning files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # umd_lerning
-学习 umd ...
+
+学习 UMD ...
+
+## UMD 简介
+
+UMD(Universal Module Definition),一种 javascript 通用模块定义规范，让你的模块能在 javascript 所有运行环境中发挥作用。
---
+++ assets/js/umd-dep/umd-module-depended.js
@@ -0,0 +1,17 @@
+(function (root, factory) {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof define === 'function' && define.cmd) {
+    define(function (require, exports, module) {
+      module.exports = factory();
+    });
+  } else {
+    root.depModule = factory();
+  }
+})(this, function () {
+  return {
+    name: '我是一个被依赖的umd模块',
+  };
+});
